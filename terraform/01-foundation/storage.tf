409266a4e7bd43f2fe7c2a55c19684941f20b494
Olorode Goodnews <olorodegoodnews797@gmail.com>
2026-09-17 15:13:39 +0100

    Remediate Trivy network security findings

--- terraform/01-foundation/storage.tf
+++ terraform/01-foundation/storage.tf
@@ -8,6 +8,7 @@ resource "azurerm_storage_account" "block2" {
   #checkov:skip=CKV_AZURE_33:Azure Queue Storage is not used by this project, so Queue service logging is not enabled.
   #checkov:skip=CKV_AZURE_206:LRS is intentionally used for this non-production student lab to conserve Azure credits.
   #checkov:skip=CKV2_AZURE_1:Customer-managed keys are not required because this lab does not store production or critical data.
+
   name                     = "stblock2${random_string.storage_suffix.result}"
   resource_group_name      = azurerm_resource_group.block2.name
   location                 = azurerm_resource_group.block2.location
@@ -38,9 +39,22 @@ resource "azurerm_storage_account" "block2" {
   }
 }
 
+resource "azurerm_storage_account_network_rules" "block2" {
+  storage_account_id = azurerm_storage_account.block2.id
+
+  default_action = "Deny"
+  bypass         = ["AzureServices"]
+}
+
 resource "azurerm_private_dns_zone" "blob" {
   name                = "privatelink.blob.core.windows.net"
   resource_group_name = azurerm_resource_group.block2.name
+
+  tags = {
+    environment = "lab"
+    managed_by  = "terraform"
+    project     = "caleb-block2"
+  }
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "blob" {
@@ -48,6 +62,13 @@ resource "azurerm_private_dns_zone_virtual_network_link" "blob" {
   resource_group_name   = azurerm_resource_group.block2.name
   private_dns_zone_name = azurerm_private_dns_zone.blob.name
   virtual_network_id    = azurerm_virtual_network.block2.id
+  registration_enabled  = false
+
+  tags = {
+    environment = "lab"
+    managed_by  = "terraform"
+    project     = "caleb-block2"
+  }
 }
 
 resource "azurerm_private_endpoint" "storage_blob" {
